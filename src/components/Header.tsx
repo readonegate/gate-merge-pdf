e0bb8c6f5b7ce7c3e643d2bef0d193491fea9350
readonegate <ridwan.gate@gmail.com>
2025-05-21 19:16:19 +0700

    [ADD] change name on header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -21,7 +21,7 @@ const Header = () => {
             <path d="M14.5 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7.5L14.5 2z" />
             <polyline points="14 2 14 8 20 8" />
           </svg>
-          <h1 className="text-xl font-bold">PDFTools</h1>
+          <h1 className="text-xl font-bold"> Gate Merge PDF </h1>
         </Link>
         <nav>
           <ul className="flex items-center gap-6">
